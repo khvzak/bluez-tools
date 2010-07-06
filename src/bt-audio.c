6720df227d66a3a200f2f5853c6ff2597af0e37a
Alexander Orlenko <zxteam@gmail.com>
2010-07-06 14:11:49 +1100

    Base impl of bt-audio

--- src/bt-audio.c
+++ src/bt-audio.c
@@ -25,8 +25,100 @@
 #include <config.h>
 #endif
 
+#include <stdlib.h>
+#include <glib.h>
+
+#include "lib/dbus-common.h"
+#include "lib/helpers.h"
+#include "lib/adapter.h"
+#include "lib/device.h"
+#include "lib/audio.h"
+
+static void audio_property_changed(Audio *audio, const gchar *name, const GValue *value, gpointer data)
+{
+	if (g_strcmp0(name, "state") == 0) {
+		if (g_strcmp0(g_value_get_string(value), "connecting") == 0) {
+			g_print("Connecting to an audio service\n");
+		} else if (g_strcmp0(g_value_get_string(value), "connected") == 0) {
+			g_print("Audio service is connected\n");
+		} else {
+			g_print("Audio service is disconnected\n");
+		}
+	}
+}
+
+static gchar *adapter_arg = NULL;
+static gchar *connect_arg = NULL;
+static gchar *disconnect_arg = NULL;
+
+static GOptionEntry entries[] = {
+	{"adapter", 'a', 0, G_OPTION_ARG_STRING, &adapter_arg, "Adapter name or MAC", NULL},
+	{"connect", 'c', 0, G_OPTION_ARG_STRING, &connect_arg, "Connect to an audio device", "device#id"},
+	{"disconnect", 'd', 0, G_OPTION_ARG_STRING, &disconnect_arg, "Disconnect from an audio device", "device#id"},
+	{NULL}
+};
+
 int main(int argc, char *argv[])
 {
+	GError *error = NULL;
+	GOptionContext *context;
 
+	g_type_init();
+
+	context = g_option_context_new("- a bluetooth generic audio manager");
+	g_option_context_add_main_entries(context, entries, NULL);
+	g_option_context_set_summary(context, "audio summary");
+	g_option_context_set_description(context, "audio desc");
+
+	if (!g_option_context_parse(context, &argc, &argv, &error)) {
+		g_print("%s: %s\n", g_get_prgname(), error->message);
+		g_print("Try `%s --help` for more information.\n", g_get_prgname());
+		exit(EXIT_FAILURE);
+	} else if (!connect_arg && !disconnect_arg) {
+		g_print("%s", g_option_context_get_help(context, FALSE, NULL));
+		exit(EXIT_FAILURE);
+	}
+
+	g_option_context_free(context);
+
+	if (!dbus_connect(&error)) {
+		g_printerr("Couldn't connect to dbus: %s", error->message);
+		exit(EXIT_FAILURE);
+	}
+
+	Adapter *adapter = find_adapter(adapter_arg, &error);
+	exit_if_error(error);
+
+	Device *device = find_device(adapter, connect_arg != NULL ? connect_arg : disconnect_arg, &error);
+	exit_if_error(error);
+
+	// TODO: Test to AUDIO service
+
+	Audio *audio = g_object_new(AUDIO_TYPE, "DBusObjectPath", device_get_dbus_object_path(device), NULL);
+	g_signal_connect(audio, "PropertyChanged", G_CALLBACK(audio_property_changed), NULL);
+
+	if (connect_arg) {
+		if (g_strcmp0(audio_get_state(audio), "connected") == 0) {
+			g_print("Audio service is already connected\n");
+		} else if (g_strcmp0(audio_get_state(audio), "connecting") == 0) {
+			//g_print("Audio service is already connecting\n");
+		} else {
+			audio_connect(audio, &error);
+			exit_if_error(error);
+		}
+	} else if (disconnect_arg) {
+		if (g_strcmp0(audio_get_state(audio), "disconnected") == 0) {
+			g_print("Audio service is already disconnected\n");
+		} else {
+			audio_disconnect(audio, &error);
+			exit_if_error(error);
+		}
+	}
+
+	g_object_unref(audio);
+	g_object_unref(device);
+	g_object_unref(adapter);
+
+	exit(EXIT_SUCCESS);
 }
 
